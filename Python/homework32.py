b353117c42c681a52518769ec868e72c5e7c3b81
Sima_molot <yul.nan345@gmail.com>
2024-07-31 22:34:00 +0200

    Added home work 32

--- Python/homework32.py
+++ Python/homework32.py
@@ -27,6 +27,94 @@ class Counter:
 
 counter = Counter(5)
 counter += 3
-print(counter)  # Вывод: 8
+print(counter)
 counter -= 2
-print(counter)  # Вывод: 6
+print(counter)
+#
+#
+# 2. Реализовать класс Email, представляющий электронное письмо. Класс должен поддерживать следующие операции:
+# - Сравнение писем по дате (операторы <, >, <=, >=, ==, !=)
+# - Преобразование письма в строку (метод __str__)
+# - Получение длины текста письма (метод __len__)
+# - Получение хеш-значения письма (метод __hash__)
+# - Проверка наличия текста в письме (метод __bool__)
+#
+# Пример использования:
+# email1 = Email("john@example.com", "jane@example.com", "Meeting", "Hi Jane, let's have a meeting tomorrow.", "2022-05-10")
+# email2 = Email("jane@example.com", "john@example.com", "Re: Meeting", "Sure, let's meet at 2 PM.", "2022-05-10")
+# email3 = Email("alice@example.com", "bob@example.com", "Hello", "Hi Bob, how are you?", "2022-05-09")
+#
+# print(email1)  # Вывод:
+#
+# """
+# From: john@example.com
+# To: jane@example.com
+# Subject: Meeting
+#
+#
+# Hi Jane, let's have a meeting tomorrow.
+#
+# """
+#
+#
+# print(len(email2))  # Вывод: 24
+#
+# print(hash(email3))  # Вывод: -920444056
+#
+# print(bool(email1))  # Вывод: True
+#
+# print(email2 > email3)  # Вывод: True
+
+class Email:
+    def __init__(self, from_email, to_email, subject, text, date):
+        self.subject = subject
+        self.date = date
+        self.text = text
+        self.to_email = to_email
+        self.from_email = from_email
+
+    def __str__(self):
+        return f'From {self.from_email}\nto {self.to_email}\nsubject {self.subject}\n \n{self.text}\n'
+
+    def __len__(self):
+        return len(self.text)
+
+    def __bool__(self):
+        return bool(self.text)
+
+    def __hash__(self):
+        return hash(self.text)
+
+    def __eq__(self, other):
+        return self.date == other.date
+
+    def __ne__(self, other):
+        return self.date != other.date
+
+    def __lt__(self, other):
+        return self.date < other.date
+
+    def __le__(self, other):
+        return self.date <= other.date
+
+    def __gt__(self, other):
+        return self.date > other.date
+
+    def __ge__(self, other):
+        return self.date >= other.date
+
+
+email1 = Email("john@example.com", "jane@example.com", "Meeting", "Hi Jane, let's have a meeting tomorrow.",
+              "2022-05-10")
+print(email1)
+email2 = Email("jane@example.com", "john@example.com", "Re: Meeting", "Sure, let's meet at 2 PM.", "2022-05-10")
+print(email2)
+email3 = Email("alice@example.com", "bob@example.com", "Hello", "Hi Bob, how are you?", "2022-05-09")
+print(email3)
+print(len(email2))  # Вывод: 24
+
+print(hash(email3))  # Вывод: -920444056
+
+print(bool(email1))  # Вывод: True
+
+print(email2 > email3)  # Вывод: True
