9959f6f59b2f883ba1d31977121a102211e346bc
Sima_molot <yul.nan345@gmail.com>
2024-08-05 15:35:10 +0200

    Update

--- Python/homework37.py
+++ Python/homework37.py
@@ -9,3 +9,44 @@
 #
 # Программа должна вывести все имена из таблицы users, дать пользователю выбрать одно из них и вывести все покупки
 # этого пользователя.
+
+import mysql.connector
+
+# #
+dbconfig = {
+    'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
+    'user': 'ich1',
+    'password': 'password',
+    'database': 'ich_edit'
+}
+user = input('Введите название таблицы: ')
+try:
+    connection = mysql.connector.connect(**dbconfig)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {user}")
+    result = cursor.fetchall()
+    print(result)
+    cursor.close()
+    connection.close()
+except mysql.connector.errors.ProgrammingError:
+    print(f'Таблицы {user}, не существует, повторите попытку')
+#
+connection = mysql.connector.connect(**dbconfig)
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM users')
+result = cursor.fetchall()
+for i in result:
+    print(i)
+into = input('Введите имя человека что бы просмотреть его заказы: ')
+cursor.execute(
+    f"""
+    select u.name, u.age, p.prod
+from users as u
+join sales as s
+on s.id = u.id
+join product as p
+on p.pid = s.pid
+where name like "{into}"
+""")
+result = cursor.fetchall()
+print(result)
